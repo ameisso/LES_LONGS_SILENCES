7eda5c5b4296029fa1045a46a32b3a8fc2ebe065
AME <ame@anomes.com>
2014-10-23 12:50:02 +0200

    New : syphon in sonar

--- sonar/application.macosx/source/sonar.java
+++ sonar/application.macosx/source/sonar.java
@@ -1,14 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import codeanticode.syphon.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class sonar extends PApplet {
+
 final int SCREEN_WIDTH  = 1024;
 final int SCREEN_HEIGHT = 768;
 
 //Syphon
-import codeanticode.syphon.*;
+
 PGraphics canvas;
 SyphonServer server;
 
 float speed = 0;
 
-void setup()
+public void setup()
 {
   size(SCREEN_WIDTH, SCREEN_HEIGHT, P2D);
   canvas = createGraphics(SCREEN_WIDTH, SCREEN_HEIGHT, P2D);
@@ -16,7 +34,7 @@ void setup()
   background(0);
 }
 
-void draw()
+public void draw()
 {
   canvas.beginDraw();
  // canvas.background(0, 0);
@@ -35,7 +53,7 @@ void draw()
   for (int i = 0; i < 4; i++)
   {
     canvas.rotate(speed);
-    speed+=0.001;
+    speed+=0.001f;
     canvas.line(0, 0, 0, 298);
   }
 
@@ -44,3 +62,12 @@ void draw()
   server.sendImage(canvas);
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "sonar" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
